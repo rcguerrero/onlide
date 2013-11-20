4b239f8f494cd677260a31fd7568b9d293f04744
nanobeto <nanobeto@gmail.com>
2013-11-20 10:40:01 +0100

    second

--- onlide/web/src/main/java/es/upm/muss/agile/webapp/action/ActiveUserList.java
+++ onlide/web/src/main/java/es/upm/muss/agile/webapp/action/ActiveUserList.java
@@ -1,5 +1,7 @@
 package es.upm.muss.agile.webapp.action;
-
+/**
+ * @author nanobeto@hotmail.com
+ */
 import es.upm.muss.agile.model.User;
 import es.upm.muss.agile.webapp.listener.UserCounterListener;
 
